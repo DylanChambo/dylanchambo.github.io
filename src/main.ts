3ede54409117ffe08c1b2bc7002542c55054f47f
DylanChambo <dylan.chambo@gmail.com>
2022-09-02 19:18:17 +1200

    feat: :sparkles: added lighting

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import './style.css'
 import * as THREE from 'three';
 
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+const DEBUG = false;
+
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000)
@@ -15,11 +19,24 @@ camera.position.setZ(30);
 renderer.render( scene, camera );
 
 const geometry = new THREE.TorusGeometry( 10, 3, 16, 100);
-const material = new THREE.MeshBasicMaterial( {color: 0xFF6347, wireframe: true } );
-const torus = new THREE.Mesh( geometry, material );
+const material = new THREE.MeshStandardMaterial( { color: 0xFF6347 } );
+const torus = new THREE.Mesh(geometry, material);
 
 scene.add( torus );
 
+const pointLight = new THREE.PointLight(0xFFFFFF);
+pointLight.position.set(5, 5, 5);
+
+const ambientLight = new THREE.AmbientLight(0xFFFFFF);
+scene.add(ambientLight, pointLight);
+if (DEBUG) {
+  const lightHelper = new THREE.PointLightHelper(pointLight);
+  const gridHelper = new THREE.GridHelper(200, 50);
+  scene.add(gridHelper, lightHelper);
+}
+
+
+const controls = new OrbitControls(camera, renderer.domElement);
 
 function animate() {
   requestAnimationFrame( animate );
@@ -28,6 +45,7 @@ function animate() {
   torus.rotation.y += 0.005;
   torus.rotation.z += 0.01;
 
+  controls.update();
 
   renderer.render( scene, camera );
 }
