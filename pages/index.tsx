3cb2d7cb8b5e8e83429e3fd5dee9fd34933ce560
DylanChambo <dylan.chambo@gmail.com>
2022-10-31 12:10:35 +1300

    feat: :sparkles: create header component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Header from '../components/Header'
 import styles from '../styles/Home.module.scss'
 
 export default function Home() {
@@ -13,6 +14,7 @@ export default function Home() {
       </Head>
 
       {/* TODO: Header */}
+      <Header />
 
       {/* TODO: Hero */}
 
