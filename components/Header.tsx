4e86031497649c7b90fb2a352cd249d4074a17c6
DylanChambo <dylan.chambo@gmail.com>
2022-10-31 12:21:58 +1300

    feat: :sparkles: add social icons

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { SocialIcon } from "react-social-icons";
 
 type Props = {}
 
 export default function Header({}: Props) {
   return (
-    <header>I am Header</header>
+    <header>
+      <div>
+        {/* Social Icons */}
+        <SocialIcon url="https://github.com/DylanChambo" fgColor='gray' bgColor='transparent'/>
+        <SocialIcon url="https://www.linkedin.com/in/dylanchambo/" fgColor='gray' bgColor='transparent'/>
+      </div>
+    </header>
   )
 }
