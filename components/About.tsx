83c81735d0691d39325aee445e6763403d3ea633
DylanChambo <dylan.chambo@gmail.com>
2022-11-12 14:27:29 +1300

    feat(about): :construction: add beggining of about section

--- components/About.tsx
+++ components/About.tsx
@@ -1,9 +1,32 @@
+import { motion } from 'framer-motion';
 import React from 'react'
+import styles from '../styles/About.module.scss';
 
 type Props = {}
 
 export default function About({}: Props) {
   return (
-    <div>About</div>
+    <div className={styles.container}>
+        <h3 className={styles.title}>About</h3>
+
+        <motion.img 
+            className={styles.image}
+            src = "dylan.png" 
+            initial = {{
+                x: -200,
+                opacity: 0,
+            }}
+            transition = {{
+                duration: 1.2,
+            }}
+            whileInView = {{ 
+                x: 0,
+                opacity: 1,
+            }}
+            viewport = {{ 
+                once: true
+            }}
+        />
+    </div>
   )
 }
