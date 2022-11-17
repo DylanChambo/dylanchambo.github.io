df2fd121bb9f659d4a7cd68e84997415c931ab3d
DylanChambo <dylan.chambo@gmail.com>
2022-11-17 15:32:18 +1300

    feat(work-experience): :lipstick: move some shared styling into a shared scss file to remove verbocity

--- components/WorkExperience.tsx
+++ components/WorkExperience.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import styles from '../styles/WorkExperience.module.scss';
+import { motion } from 'framer-motion';
 
 type Props = {}
 
 export default function WorkExperience({}: Props) {
   return (
-    <div>WorkExperience</div>
-  )
+    <motion.div
+        initial = {{ opacity: 0 }}
+        whileInView = {{ opacity: 1 }}
+        transition = {{ duration: 1.5 }}
+        className={styles.container}
+    >
+        <h3 className={styles.title}>Experience</h3>
+        <div className={styles.cardbox}>
+
+        </div>
+    </motion.div>
+  );
 }
