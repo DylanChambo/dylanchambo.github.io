313bc796cdbbafa6c2d752766418fac7864ac877
DylanChambo <dylan.chambo@gmail.com>
2023-06-08 11:36:10 +1200

    feat: :sparkles: add custom scrollbar

--- src/components/ContactMe/ContactMe.tsx
+++ src/components/ContactMe/ContactMe.tsx
@@ -21,8 +21,7 @@ export default function ContactMe({ }: Props) {
   return (
     <Section title="Contact" id='contact'>
       <h4 className={styles.header}>
-        I have got just what you need.{" "}
-        <span className={styles.underlined}>Lets Talk.</span>
+        <span className={styles.underlined}>Let's Talk.</span>
       </h4>
       <div className={styles.contacts}>
         <div className={styles.contactContainer}>
