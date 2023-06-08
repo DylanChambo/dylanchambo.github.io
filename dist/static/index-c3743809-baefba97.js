c381d7b48f3a42c85bd9d3be7fec690319629e2d
DylanChambo <dylan.chambo@gmail.com>
2023-06-08 16:27:12 +1200

    feat: :sparkles: remove dist

--- dist/static/index-c3743809-baefba97.js
+++
@@ -1,1 +0,0 @@
-import{DocumentPaneProvider as m,DocumentPane as a}from"./index-7415496d-dd8fc2aa.js";import"./sanity-9ff6e0cf.js";import"./json-inspector-27981fe6.js";export{m as DocumentPaneProvider,a as default};
